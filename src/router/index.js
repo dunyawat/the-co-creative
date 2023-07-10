5a180809361b006b576fc5dcbb9a6bcb588485aa
TaTae <eatnim@eatniM.local>
2023-07-10 22:05:46 +0700

    Test history

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ import ContactView from '../views/ContactView.vue'
 const router = createRouter({
   
 
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
